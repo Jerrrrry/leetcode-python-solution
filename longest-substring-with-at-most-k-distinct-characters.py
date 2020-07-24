fd7c0fc4e6e7fe078d79443cb97909aec33ae372
Rachel Qiu <rachel@Rachels-iMac.local>
2020-07-23 23:47:31 -0700

    yubin:update

---
+++ longest-substring-with-at-most-k-distinct-characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self,s:str,k:int)->int:
+        seen={}
+        j=0
+        res=0
+        for i in range(len(s)):
+            if s[i] not in seen:
+                seen[s[i]]=1
+            else:
+                seen[s[i]]+=1
+            while len(seen)>k:
+                seen[s[j]]-=1
+                if seen[s[j]]==0:
+                    del seen[s[j]]
+                j+=1
+            res=max(res,i-j+1)
+        return res
