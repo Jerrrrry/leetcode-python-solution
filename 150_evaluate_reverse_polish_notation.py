ab08997ac0ea6bf3f8c8e71feb40558ae8d18a0f
yubin <yubinqiu1101@gmail.com>
2021-11-21 12:04:16 -0800

    yubin:update

---
+++ 150_evaluate_reverse_polish_notation.py
@@ -0,0 +1,3 @@
+from typing import List
+class Solution:
+        def evalRPN(self,tokens:List[str])->int:
---
+++ float.py
@@ -0,0 +1,7 @@
+#l//r not equal to int(l/r)  or int(float(l)/r)
+
+print(int(-9//2))
+
+
+print(int(-9/2))
+##support int(l/r) and int(float(l)/r) truncate toward  zero
